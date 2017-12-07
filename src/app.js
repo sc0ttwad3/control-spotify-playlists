25919297b2249b3e20c8fdddc9bfd70a5a5577cd
Scott Wade <sc0ttwad3@gmail.com>
2017-12-07 00:09:21 -0500

    Comment out getHashParams section until find way to access url hashcode

--- src/app.js
+++ src/app.js
@@ -34,14 +34,23 @@ const generateRandomString = (length) => {
 */
 const getHashParams = () => {
  var hashParams = {};
+/*
+
+  window.location isn't available here!
+
  var e, r = /([^&;=]+)=?([^&;]*)/g,
      q = window.location.hash.substring(1);
  while ( e = r.exec(q)) {
     hashParams[e[1]] = decodeURIComponent(e[2]);
  }
+ */
  return hashParams;
 }
 
+let params = getHashParams();
+let access_token = params.access_token;
+let refresh_token = params.refresh_token;
+let error = params.error;
 
 let stateKey = 'spotify_auth_state';
 
